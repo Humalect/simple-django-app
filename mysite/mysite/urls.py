6bdc1310d5cc2fcab71743581372ee931ac547b5
zriyansh <prik2two@gmail.com>
2023-03-30 03:08:21 +0530

    fix

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path, include     # <--  ADD THIS LINE
+from django.urls import path, include    
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('hello_world.urls')),   # <--  ADD THIS LINE
+    path('', include('hello_world.urls')),   
 ]
